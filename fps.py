e8fbcd34c25dea309e8d3a24f811a7685b44c66a
Shlomo Zippel <shlomo.zippel@gmail.com>
2024-03-13 21:10:13 -0700

    fixed fps code

--- fps.py
+++ fps.py
@@ -3,7 +3,6 @@ import time
 
 class FPS:
     def __init__(self, verbose=False):
-        self._accum = 0
         self._count = 0
         self._last_calc = 0
         self._fps = 0
@@ -12,12 +11,13 @@ class FPS:
     def tick(self):
         now = time.ticks_ms()
         delta = time.ticks_diff(now, self._last_calc)
-        self._accum += delta
         self._count += 1
 
-        if time.ticks_diff(now, self._last_calc) > 1000:
-            self._fps = 1000 / (self._accum / self._count)
-            self._accum = 0
+        if  delta >= 1000:
+            # print("Ticks diff: ", ticks_diff)
+            # print("Accum: ", self._accum)
+            # print("Count: ", self._count)
+            self._fps = self._count / (delta / 1000)
             self._count = 0
             self._last_calc = now
 
